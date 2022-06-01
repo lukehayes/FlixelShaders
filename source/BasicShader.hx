69cc6a2da562cf8a375befa74a1ec991c4c0f120
Luke Hayes <lukepurplehayes@gmail.com>
2022-06-01 17:13:35 +0100

    Updated BasicShader with glsl random noise etc.

--- source/BasicShader.hx
+++ source/BasicShader.hx
@@ -9,19 +9,29 @@ class BasicShader extends FlxShader
     @:glFragmentSource('
      #pragma header
 
+
+    float rand(float n){return fract(sin(n) * 43758.5453123);}
+
+
+     uniform float u_time;
+
       void main()
       {
-          gl_FragColor = vec4(1,0,0,1);
+
+          vec2 st = gl_FragCoord.xy;
+          float res = smoothstep(st.x, st.y, sin(u_time) * 100.0);
+          gl_FragColor = vec4(rand(st.x/1.0),res / 2.0,st.y,1);
       }
     ')
 
     public function new()
     {
         super();
+        this.u_time.value = [0.0];
     }
 
     public function update(elapsed:Float)
     {
-        trace("Updating Shader");
+        this.u_time.value[0] += elapsed;
     }
 }
